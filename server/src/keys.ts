07e471db1c8689b054d05cae6481e39013459b79
Bayayo <felizoide@gmail.com>
2020-04-01 16:45:50 -0600

    comentarios de conexión

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -3,7 +3,7 @@ export default {
     database:{
         host: 'localhost',
         user: 'root',
-        pass: 'MySqlPass0123',
-        database: 'ng_games_db'
+        pass: 'MySqlPass0123', //Tu pass local
+        database: 'ng_games_db' //Se tenia que haber ejecutado el SQL de la carpeta database
     }
 }
